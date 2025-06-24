6df1e67f0138b403c37f65530691c5cb98986e71
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-06-24 11:04:40 +0530

    done

--- Backend/main.py
+++ Backend/main.py
@@ -32,7 +32,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="AI INTERVIEWER")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","http://13.204.76.229:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],)
