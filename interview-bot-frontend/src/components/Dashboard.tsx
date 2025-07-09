608d5cad2630eeca9f32b7b099a2cb072faef560
ranjan2829 <ranjan.shitole3129@gmail.com>
2025-07-09 12:16:49 +0530

    done

--- interview-bot-frontend/src/components/Dashboard.tsx
+++ interview-bot-frontend/src/components/Dashboard.tsx
@@ -327,7 +327,7 @@ const Dashboard: React.FC = () => {
         body: JSON.stringify({
           title: interviewFormData.title,
           role: interviewFormData.role,
-          candidateName: candidateData.candidate_name,
+          candidateName:interviewFormData.candidateName,
           candidateEmail: interviewFormData.candidateEmail,
           resume: interviewFormData.resume,
           jobDescription: interviewFormData.jobDescription,
